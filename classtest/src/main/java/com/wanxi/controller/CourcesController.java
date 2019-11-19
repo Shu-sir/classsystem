5b0b5bb0ab63b11a4779eb3cd27aee810c0eebe1
Shu-sir <3262700386@qq.com>
2019-11-19 21:37:15 +0800

    第一次测试提交班级系统代码

--- classtest/src/main/java/com/wanxi/controller/CourcesController.java
+++ classtest/src/main/java/com/wanxi/controller/CourcesController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 /**
- * 我正在测试分支合并
+ * 我正在测试dev分支合并
  * @author ：MR.Shu
  * @date ：Created in 2019/11/17 11:54
  * @description：
