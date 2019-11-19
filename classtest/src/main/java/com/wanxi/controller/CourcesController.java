a0af029e4857baec8ea3a07c25454a7a0b77c866
Shu-sir <3262700386@qq.com>
2019-11-19 21:42:41 +0800

    第一次测试提交班级系统代码

--- classtest/src/main/java/com/wanxi/controller/CourcesController.java
+++ classtest/src/main/java/com/wanxi/controller/CourcesController.java
@@ -3,7 +3,6 @@ package com.wanxi.controller;
 import com.wanxi.entiry.CoursesEntity;
 import com.wanxi.service.CoursesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
